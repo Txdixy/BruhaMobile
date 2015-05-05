3b523a77f29a0f2326e2e7b7ba7e1aedd0330137
Yunfei Cui <y35cui@uwaterloo.ca>
2015-05-04 22:07:18 -0400

    Day 1
    setup splash page
    login info

---
+++ BruhaMobile/SplashViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  BruhaMobile
+//
+//  Created by YUNFEI CUI on 2015-05-04.
+//  Copyright (c) 2015 Bruha. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
+
